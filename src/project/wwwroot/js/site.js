8d6f63893b8bb135d0f4e76017911ad9db068282
Nygel Melchor <nymelchor@protonmail.com>
2023-02-10 14:47:42 -0800

    Create hero content for Index page

--- src/project/wwwroot/js/site.js
+++ src/project/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(window).scroll(function () {
+    $('nav').toggleClass('scrolled box-shadow', $(this).scrollTop() > 0);
+});
