4ce3ada3a5db1e3dece205c1dc6757146155fce4
Ewest19 <Ewest19@mail.wou.edu>
2023-02-18 21:51:59 -0800

    Added simple view and controller for profile page

---
+++ src/project/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WatchParty.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        
+    }
+}
