da47b36f15cb9f134a6892fefdc8b71a569c2782
Ewest19 <Ewest19@mail.wou.edu>
2023-02-19 16:54:32 -0800

    Changed up and seed scripts to match new data model

--- src/project/Controllers/UserController.cs
+++ src/project/Controllers/UserController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WatchParty.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        //[Authorize]
         public IActionResult Profile()
         {
+            string Username = User.Identity.Name ?? "Not found";
+            ViewBag.Username = Username;
+
             return View();
         }
 
