9a6f2f9f3fc8e0a01bbc03284dc16708445f3ef0
Armando Yanez <ayanez20@wou.edu>
2023-02-26 20:15:55 -0800

    rescaffolded models from updated database

--- src/project/Models/LikePost.cs
+++ src/project/Models/LikePost.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace WatchParty.Models;
 
+[Table("LikePost")]
 public partial class LikePost
 {
+    [Key]
+    [Column("ID")]
     public int Id { get; set; }
 
+    [Column("PostID")]
     public int PostId { get; set; }
 
+    [Column("UserID")]
     public int UserId { get; set; }
 
+    [ForeignKey("PostId")]
+    [InverseProperty("LikePosts")]
     public virtual Post Post { get; set; } = null!;
 
+    [ForeignKey("UserId")]
+    [InverseProperty("LikePosts")]
     public virtual Watcher User { get; set; } = null!;
 }
