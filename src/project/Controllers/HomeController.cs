ecd9b1411e1978ef5f37fb238ee1b7929ca0a8e9
Armando Yanez <ayanez20@wou.edu>
2023-03-09 15:53:44 -0800

    created page to search for users and connected it to partial for logged in user and connected it from the Home Controller

--- src/project/Controllers/HomeController.cs
+++ src/project/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult FindUsers()
+    {
+        return View();
+    }
+
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
