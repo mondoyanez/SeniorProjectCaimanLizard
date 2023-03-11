910d04907b4e1245a7226a7d4d68698b861f99db
Nygel Melchor <nymelchor@protonmail.com>
2023-03-10 18:14:40 -0800

    Implement SendGrid Service class

---
+++ src/project/Services/Concrete/SendGridService.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace WatchParty.Services.Concrete
+{
+    public class SendGridService : IEmailSender
+    {
+        public string? Key { get; set; }
+
+        public SendGridService(string? key)
+        {
+            Key = key;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            if (string.IsNullOrEmpty(Key))
+            {
+                throw new Exception("Null SendGridKey");
+            }
+
+            await Execute(Key, subject, message, toEmail);
+        }
+
+        public async Task Execute(string apiKey, string subject, string message, string toEmail)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("Joe@contoso.com", "Password Recovery"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(toEmail));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine(response.IsSuccessStatusCode
+                                ? $"Email to {toEmail} queued successfully!"
+                                : $"Failure Email to {toEmail}");
+        }
+    }
+}
