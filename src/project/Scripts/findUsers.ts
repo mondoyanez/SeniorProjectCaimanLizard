a708ef4f332e72d4690b23c9b6007ba4f7a05710
Armando Yanez <ayanez20@wou.edu>
2023-03-12 17:01:22 -0700

    added validation checking when user enters a username

--- src/project/Scripts/findUsers.ts
+++ src/project/Scripts/findUsers.ts
@@ -1,5 +1,20 @@
 ﻿const submitButton = $("#username-search-btn");
+const re = new RegExp("^[A-Za-z]+$");
+//const query = $("#username-entered").val();
 
 submitButton.on("click", () => {
-    console.log("button pressed");
+    $("#user-search-input-error-message").empty();
+
+    if ($("#username-entered").val().toString() === "") {
+        $("#user-search-input-error-message").addClass("user-search-error-rq");
+        $("#user-search-input-error-message").append("<p>This is a required field, please enter a username</p>");
+        return;
+    }
+
+    if (!re.test($("#username-entered").val().toString())) {
+        $("#user-search-input-error-message").addClass("user-search-error-rq");
+        $("#user-search-input-error-message").append("<p>Input can only contain alphabetic characters</p>");
+        return;
+    }
+    $("#user-search-input-error-message").append("<p>VALID INPUT</p>");
 });
--- src/project/wwwroot/js/findUsers.js
+++ src/project/wwwroot/js/findUsers.js
@@ -1,5 +1,18 @@
 var submitButton = $("#username-search-btn");
+var re = new RegExp("^[A-Za-z]+$");
+//const query = $("#username-entered").val();
 submitButton.on("click", function () {
-    console.log("button pressed");
+    $("#user-search-input-error-message").empty();
+    if ($("#username-entered").val().toString() === "") {
+        $("#user-search-input-error-message").addClass("user-search-error-rq");
+        $("#user-search-input-error-message").append("<p>This is a required field, please enter a username</p>");
+        return;
+    }
+    if (!re.test($("#username-entered").val().toString())) {
+        $("#user-search-input-error-message").addClass("user-search-error-rq");
+        $("#user-search-input-error-message").append("<p>Input can only contain alphabetic characters</p>");
+        return;
+    }
+    $("#user-search-input-error-message").append("<p>VALID INPUT</p>");
 });
 //# sourceMappingURL=findUsers.js.map
