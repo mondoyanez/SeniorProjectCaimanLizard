ce964b2f8ee6efff3a2927e437d35937cdda2a8e
Armando Yanez <ayanez20@wou.edu>
2023-03-19 15:38:04 -0700

    amount of followers now being updated at runtime without page refresh still create logic so that follow saves to database

--- src/project/wwwroot/dist/followUser.js
+++ src/project/wwwroot/dist/followUser.js
@@ -2,10 +2,12 @@
 const submitButton = $("#user-profile-btn-follow");
 submitButton.on("click", (e) => {
     e.preventDefault();
-    changeButtonToFollowing();
+    addFollower();
 });
-function changeButtonToFollowing() {
+function addFollower() {
     $("#user-profile-btn-follow").hide();
     $("#user-profile-following-icon").show();
+    // Get current amount of followers, convert that into a number add 1 more follower and save the new result into user-amount-followers
+    $("#user-amount-followers").html(String(Number($("#user-amount-followers").html()) + 1));
 }
 //# sourceMappingURL=followUser.js.map
