36cbb8eabd285f5f2eb61e3bf686331b8dc00279
Nygel Melchor <nymelchor@protonmail.com>
2023-03-19 10:25:51 -0700

    Edit callback url and route values for email verification link

--- src/project/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/project/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -151,11 +151,11 @@ namespace WatchParty.Areas.Identity.Pages.Account
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        values: new { userId = userId, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "",
+                        $"{{\"username\": \"{Input.UserName}\", \"callbackurl\": \"{callbackUrl}\", \"expiredate\": \"{DateTime.Now.AddDays(1).ToString("MMMM d, yyyy h:mm tt")}\"}}");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
