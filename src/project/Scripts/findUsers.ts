7b036de4af105d9bc7824ddf8e21471b6322e197
Armando Yanez <ayanez20@wou.edu>
2023-03-19 00:26:57 -0700

    stylized links in users table returned during search

--- src/project/Scripts/findUsers.ts
+++ src/project/Scripts/findUsers.ts
@@ -38,7 +38,7 @@ function findUsers(data: any[]) {
                 const result: string =
                     `
                 <tr>
-                    <td><a href="/user/${item.username}">${item.username}</a></td>
+                    <td><a class="user-profile-link" href="/user/${item.username}">${item.username}</a></td>
                     <td>${item.email ?? ""}</td>
                     <td>${item.firstName ?? ""} ${item.lastName ?? ""}</td>
                     <td>${item.followingCount ?? 0}</td>
