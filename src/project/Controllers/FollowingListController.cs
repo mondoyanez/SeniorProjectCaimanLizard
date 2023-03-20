d75238a22399901f89fd95a51a13ddb9d7f6475c
Armando Yanez <ayanez20@wou.edu>
2023-03-20 11:27:56 -0700

    removed usermangager from FollowingListController because it was never going to be used

--- src/project/Controllers/FollowingListController.cs
+++ src/project/Controllers/FollowingListController.cs
@@ -10,18 +10,16 @@ namespace WatchParty.Controllers;
 [ApiController]
 public class FollowingListController : ControllerBase
 {
-    private readonly UserManager<IdentityUser> _userManager;
     private readonly IWatcherRepository _watcherRepository;
     private readonly IFollowingListRepository _followingListRepository;
-    public FollowingListController(UserManager<IdentityUser> userManager, IWatcherRepository watcherRepository, IFollowingListRepository followingListRepository)
+    public FollowingListController(IWatcherRepository watcherRepository, IFollowingListRepository followingListRepository)
     {
-        _userManager = userManager;
         _watcherRepository = watcherRepository;
         _followingListRepository = followingListRepository;
     }
 
     [HttpPost("addFollower")]
-    public IActionResult AddFollower([FromBody] int followerId)
+    public IActionResult AddFollower(int followerId)
     {
         Watcher? loggedInWatcher = _watcherRepository.FindByUsername(User.Identity.Name);
 
