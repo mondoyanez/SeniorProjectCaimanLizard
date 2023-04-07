d5a5deb79afa7296155c4615b22066d8f9009c85
Armando Yanez <ayanez20@wou.edu>
2023-04-07 16:58:36 -0700

    implementated GetComments from CommentRepository, added it to FeedVM, and added dependency injection for CommentRepository for testing later and made modifications to PostController to include Comments to be used in Index later

---
+++ src/project/DAL/Concrete/CommentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WatchParty.DAL.Abstract;
+using WatchParty.Models;
+
+namespace WatchParty.DAL.Concrete;
+public class CommentRepository: Repository<Comment>, ICommentRepository
+{
+    public CommentRepository(WatchPartyDbContext ctx) : base(ctx)
+    {
+    }
+
+    public IEnumerable<Comment> GetComments()
+    {
+        IEnumerable<Comment> comments = GetAll().OrderBy(c => c.DatePosted).ToList();
+
+        if (!comments.Any())
+            throw new Exception("No comments were found");
+
+        return comments;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+}
