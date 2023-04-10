87a2105401adc3c44685a17286e36447a8bd2de6
Armando Yanez <ayanez20@wou.edu>
2023-04-09 20:40:51 -0700

    wrote test to validate if CommentTitle in Comment model exceeds length

--- src/nunit/CommentsModelValidator_Tests.cs
+++ src/nunit/CommentsModelValidator_Tests.cs
@@ -101,4 +101,42 @@ public class CommentsModelValidator_Tests
             Assert.That(actual, Is.EqualTo("This text contains a comment"));
         });
     }
+
+    [Test]
+    public void Comment_WithTitleExceedingLength_IsNotValid()
+    {
+        // Arrange
+        Comment comment = MakeValidComment();
+        comment.CommentTitle =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Fusce blandit ligula ut neque ultricies convallis. " +
+            "Pellentesque in ipsum eget metus venenatis vestibulum. Morbi ut velit sed nibh pharetra bibendum eget ac augue. Praesent suscipit " +
+            "elit eu arcu consectetur rhoncus. Curabitur feugiat dolor eu tellus euismod, eget lobortis leo bibendum. Suspendisse euismod neque eget " +
+            "metus dapibus, eget bibendum libero elementum. Donec pulvinar velit eget mi lobortis, a luctus turpis sollicitudin. Vivamus mollis felis " +
+            "id est congue faucibus. Sed sit amet blandit ipsum. Etiam sagittis, turpis at fringilla consectetur, ipsum augue auctor tellus, non " +
+            "imperdiet sapien magna in mauris. Aenean euismod ante enim, at volutpat libero dictum ac. Vestibulum bibendum eros ac tristique finibus. " +
+            "Proin auctor feugiat metus, eu aliquam felis. Integer ac turpis vel elit pellentesque rhoncus. Vestibulum vel accumsan ex. Vestibulum " +
+            "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis laoreet odio eget dui malesuada, auctor bibendum nisl " +
+            "maximus. Sed hendrerit odio non massa sagittis, euismod suscipit augue ullamcorper. Vestibulum lacinia nulla nulla, ut euismod velit " +
+            "viverra non. Donec bibendum quam at ligula gravida, ut scelerisque mi posuere. Fusce luctus posuere felis, nec volutpat quam interdum at. " +
+            "Maecenas faucibus ex quis arcu commodo, eget aliquet sapien mollis. Sed eu nibh convallis, vehicula arcu eu, suscipit ex. Suspendisse " +
+            "potenti. Ut sed eros vel nisl venenatis tincidunt nec eget orci. Nullam scelerisque, tortor in consectetur bibendum, ante mauris rutrum " +
+            "metus, sit amet interdum mauris tellus vitae ex. Nulla pretium odio quam, vitae venenatis nisl luctus eget. Sed id leo quis libero " +
+            "congue pretium. Nullam quis imperdiet magna. Etiam dignissim congue faucibus. Nam vitae euismod libero, ut dignissim velit. Aliquam sit " +
+            "amet nunc eu felis ullamcorper facilisis in vitae lectus. Sed faucibus mi ac ligula fermentum, non dignissim elit tincidunt. Pellentesque " +
+            "sit amet justo auctor, tristique ante quis, imperdiet magna. Aliquam non fringilla orci. Suspendisse non augue euismod, vestibulum ex in, " +
+            "tristique magna. Maecenas vel orci ac libero blandit cursus. Aliquam tincidunt turpis mauris, ac pulvinar orci dictum a. Nam sed erat " +
+            "sapien. Nunc euismod venenatis vestibulum. Nulla facilisi. Vivamus a nunc et felis rhoncus consequat. Sed a bibendum dolor, nec " +
+            "consectetur dolor. Nulla aliquam sapien vel mi eleifend, sit amet efficitur lectus commodo. Pellentesque ultrices ex ac nulla dictum, " +
+            "eu aliquet velit euismod. In nec";
+
+        // Act
+        ModelValidator mv = new ModelValidator(comment);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(mv.Valid, Is.False);
+            Assert.That(mv.ContainsFailureFor("CommentTitle"), Is.True);
+        });
+    }
 }
