5da09e181559f8f77a32008a0e242af3b28ab718
Armando Yanez <ayanez20@wou.edu>
2023-04-19 17:51:51 -0700

    created unfollow functionality to FollowingListController

--- src/project/Scripts/followUser.ts
+++ src/project/Scripts/followUser.ts
@@ -1,6 +1,7 @@
-﻿const submitButton: JQuery<HTMLElement> = $("#user-profile-btn-follow");
+﻿const followButton: JQuery<HTMLElement> = $("#user-profile-btn-follow");
+const unfollowButton: JQuery<HTMLElement> = $("#user-profile-following-icon");
 
-submitButton.on("click", (e: JQuery.Event) => {
+followButton.on("click", (e: JQuery.Event) => {
     e.preventDefault();
     const followerUsername = $("#user-profile-username").val();
 
@@ -15,11 +16,34 @@ submitButton.on("click", (e: JQuery.Event) => {
     });
 });
 
+unfollowButton.on("click", (e: JQuery.Event) => {
+    e.preventDefault();
+    console.log("unfollow button pressed");
+    const followerUsername = $("#user-profile-username").val();
+
+    $(() => {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: `/followingList/removeFollower/?followerUsername=${followerUsername}`,
+            success: removeFollower,
+            error: errorOnAjax
+        });
+    });
+});
+
 function addFollower(): void {
     $("#user-profile-btn-follow").hide();
     $("#user-profile-following-icon").show();
     // Get current amount of followers, convert that into a number add 1 more follower and save the new result into user-amount-followers
     $("#user-amount-followers").html(String(Number($("#user-amount-followers").html()) + 1));
+}
+
+function removeFollower(): void {
+    $("#user-profile-following-icon").hide();
+    $("#user-profile-btn-follow").show();
+    // Get current amount of followers, convert that into a number subtract 1 follower and save the new result into user-amount-followers
+    $("#user-amount-followers").html(String(Number($("#user-amount-followers").html()) - 1));
 }
 
 function errorOnAjax() {
--- src/project/wwwroot/dist/followUser.js
+++ src/project/wwwroot/dist/followUser.js
@@ -1,6 +1,7 @@
 "use strict";
-const submitButton = $("#user-profile-btn-follow");
-submitButton.on("click", (e) => {
+const followButton = $("#user-profile-btn-follow");
+const unfollowButton = $("#user-profile-following-icon");
+followButton.on("click", (e) => {
     e.preventDefault();
     const followerUsername = $("#user-profile-username").val();
     $(() => {
@@ -13,11 +14,31 @@ submitButton.on("click", (e) => {
         });
     });
 });
+unfollowButton.on("click", (e) => {
+    e.preventDefault();
+    console.log("unfollow button pressed");
+    const followerUsername = $("#user-profile-username").val();
+    $(() => {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: `/followingList/removeFollower/?followerUsername=${followerUsername}`,
+            success: removeFollower,
+            error: errorOnAjax
+        });
+    });
+});
 function addFollower() {
     $("#user-profile-btn-follow").hide();
     $("#user-profile-following-icon").show();
     // Get current amount of followers, convert that into a number add 1 more follower and save the new result into user-amount-followers
     $("#user-amount-followers").html(String(Number($("#user-amount-followers").html()) + 1));
+}
+function removeFollower() {
+    $("#user-profile-following-icon").hide();
+    $("#user-profile-btn-follow").show();
+    // Get current amount of followers, convert that into a number subtract 1 follower and save the new result into user-amount-followers
+    $("#user-amount-followers").html(String(Number($("#user-amount-followers").html()) - 1));
 }
 function errorOnAjax() {
     console.log("ERROR in ajax request");
