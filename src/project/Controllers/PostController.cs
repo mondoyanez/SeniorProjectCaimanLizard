2b7bb5b0dc5d0bcbce34fdbfe0f2afc03832b01d
Armando Yanez <ayanez20@wou.edu>
2023-04-21 16:28:40 -0700

    deleted commented out code in PostController

--- src/project/Controllers/PostController.cs
+++ src/project/Controllers/PostController.cs
@@ -78,9 +78,9 @@ public class PostController : Controller
         ModelState.Clear();
         TryValidateModel(post);
 
-        if (!ModelState.IsValid) 
+        if (!ModelState.IsValid)
             return View(post);
-        
+
         try
         {
             _postRepository.AddPost(post);
@@ -93,100 +93,4 @@ public class PostController : Controller
 
         return RedirectToAction("Index");
     }
-    /*
-    // GET: Post/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null || _postRepository.Posts == null)
-        {
-            return NotFound();
-        }
-
-        var post = await _postRepository.Posts.FindAsync(id);
-        if (post == null)
-        {
-            return NotFound();
-        }
-        ViewData["UserId"] = new SelectList(_postRepository.Watchers, "Id", "Id", post.UserId);
-        return View(post);
-    }
-
-    // POST: Post/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,PostTitle,PostDescription,DatePosted,UserId")] Post post)
-    {
-        if (id != post.Id)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _postRepository.Update(post);
-                await _postRepository.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PostExists(post.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        ViewData["UserId"] = new SelectList(_postRepository.Watchers, "Id", "Id", post.UserId);
-        return View(post);
-    }
-
-    // GET: Post/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null || _postRepository.Posts == null)
-        {
-            return NotFound();
-        }
-
-        var post = await _postRepository.Posts
-            .Include(p => p.User)
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (post == null)
-        {
-            return NotFound();
-        }
-
-        return View(post);
-    }
-
-    // POST: Post/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        if (_postRepository.Posts == null)
-        {
-            return Problem("Entity set 'WatchPartyDbContext.Posts'  is null.");
-        }
-        var post = await _postRepository.Posts.FindAsync(id);
-        if (post != null)
-        {
-            _postRepository.Posts.Remove(post);
-        }
-            
-        await _postRepository.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-
-    private bool PostExists(int id)
-    {
-        return (_postRepository.Posts?.Any(e => e.Id == id)).GetValueOrDefault();
-    }*/
 }
