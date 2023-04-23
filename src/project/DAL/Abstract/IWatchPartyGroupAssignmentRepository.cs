5c2c0e28dacef46405e7c55075621757dcedf19b
Armando Yanez <ayanez20@wou.edu>
2023-04-23 11:42:28 -0700

    created interface and class for WatchPartyGroupAssignmentRepoistory still needs to be implmentated and tested

---
+++ src/project/DAL/Abstract/IWatchPartyGroupAssignmentRepository.cs
@@ -0,0 +1,7 @@
+﻿using WatchParty.Models;
+
+namespace WatchParty.DAL.Abstract;
+public interface IWatchPartyGroupAssignmentRepository : IRepository<WatchPartyGroupAssignment>
+{
+    void AddToGroup(WatchPartyGroupAssignment assignment);
+}
---
+++ src/project/DAL/Concrete/WatchPartyGroupAssignmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using WatchParty.DAL.Abstract;
+using WatchParty.Models;
+
+namespace WatchParty.DAL.Concrete;
+public class WatchPartyGroupAssignmentRepository : Repository<WatchPartyGroupAssignment>, IWatchPartyGroupAssignmentRepository
+{
+    public WatchPartyGroupAssignmentRepository(WatchPartyDbContext ctx) : base(ctx)
+    {
+    }
+
+    public void AddToGroup(WatchPartyGroupAssignment assignment)
+    {
+        throw new NotImplementedException();
+    }
+}
