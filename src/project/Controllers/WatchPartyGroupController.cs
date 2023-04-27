90aa1a01517ced5dcf218f2bfc3b6ec4e30591f7
Armando Yanez <ayanez20@wou.edu>
2023-04-26 17:19:30 -0700

    Created controller for WatchPartyGroup and index view and added button in user profile to take user to page to create the group

---
+++ src/project/Controllers/WatchPartyGroupController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WatchParty.DAL.Abstract;
+
+namespace WatchParty.Controllers;
+
+[Authorize]
+public class WatchPartyGroupController : Controller
+{
+    private readonly IWatchPartyGroupRepository _groupRepository;
+    private readonly IWatchPartyGroupAssignmentRepository _assignmentRepository;
+    private readonly IWatcherRepository _watcherRepository;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public WatchPartyGroupController(IWatchPartyGroupRepository groupRepository, IWatchPartyGroupAssignmentRepository assignmentRepository, IWatcherRepository watcherRepository, UserManager<IdentityUser> userManager)
+    {
+        _groupRepository = groupRepository;
+        _assignmentRepository = assignmentRepository;
+        _watcherRepository = watcherRepository;
+        _userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
