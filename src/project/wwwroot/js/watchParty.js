62570255261524100be17464464877b51b539045
Armando Yanez <ayanez20@wou.edu>
2023-04-30 16:08:39 -0700

    implementated client side code to add functionality to add users to group and then remove the button after clicking in Details page of WatchPartyGroupController

---
+++ src/project/wwwroot/js/watchParty.js
@@ -0,0 +1,23 @@
+﻿const groupId = $("#watch-group-id").val();
+const listUsers = $("#watch-group-users")[0];
+
+$(".add-user").on("click", function (e) {
+    e.preventDefault();
+    const userId = e.target.value;
+
+    addUserToTable(e);
+    removeButton(e);
+});
+
+function addUserToTable(e) {
+    const username = e.target.innerHTML;
+
+    const element = document.createElement("p");
+    element.innerHTML = username;
+
+    listUsers.append(element);
+}
+
+function removeButton(e) {
+    e.target.parentNode.remove();
+}
