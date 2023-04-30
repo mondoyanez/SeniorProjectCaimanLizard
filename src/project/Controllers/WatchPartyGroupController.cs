5b0ac6ffe7155a545cad11bf581d9ccff65fa83a
Armando Yanez <ayanez20@wou.edu>
2023-04-29 18:05:26 -0700

    implementated post request for submitting creation of group in WatchPartyGroupController

--- src/project/Controllers/WatchPartyGroupController.cs
+++ src/project/Controllers/WatchPartyGroupController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WatchParty.DAL.Abstract;
+using WatchParty.Models;
 
 namespace WatchParty.Controllers;
 
@@ -9,21 +9,45 @@ namespace WatchParty.Controllers;
 public class WatchPartyGroupController : Controller
 {
     private readonly IWatchPartyGroupRepository _groupRepository;
-    private readonly IWatchPartyGroupAssignmentRepository _assignmentRepository;
     private readonly IWatcherRepository _watcherRepository;
-    private readonly UserManager<IdentityUser> _userManager;
 
-    public WatchPartyGroupController(IWatchPartyGroupRepository groupRepository, IWatchPartyGroupAssignmentRepository assignmentRepository, IWatcherRepository watcherRepository, UserManager<IdentityUser> userManager)
+    public WatchPartyGroupController(IWatchPartyGroupRepository groupRepository, IWatcherRepository watcherRepository)
     {
         _groupRepository = groupRepository;
-        _assignmentRepository = assignmentRepository;
         _watcherRepository = watcherRepository;
-        _userManager = userManager;
     }
 
     [HttpGet]
     public IActionResult Create()
     {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([Bind("GroupTitle, GroupDescription, StartDate")] WatchPartyGroup newGroup)
+    {
+        if (newGroup.StartDate.Equals(new DateTime(1, 1, 1, 0, 0, 0)))
+            newGroup.StartDate = new DateTime(DateTime.Now.AddDays(7).Year, DateTime.Now.AddDays(7).Month, DateTime.Now.AddDays(7).Day, 20, 0, 0);
+
+        ModelState.Clear();
+
+        Watcher? watcher = _watcherRepository.FindByUsername(User?.Identity?.Name);
+
+        if (watcher == null)
+            throw new ArgumentException(nameof(watcher));
+
+        newGroup.HostId = watcher.Id;
+        newGroup.Host = watcher;
+
+        TryValidateModel(newGroup);
+
+        if (ModelState.IsValid)
+        {
+            _groupRepository.CreateWatchPartyGroup(newGroup);
+            return RedirectToAction("Profile", "User", new { username = User.Identity.Name });
+        }
+
         return View();
     }
 }
