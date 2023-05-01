9086c225f7117680808883c3bbd4a6a83ff01c91
Armando Yanez <ayanez20@wou.edu>
2023-04-30 19:42:06 -0700

    when no more users to add to group modal now displays text no users to add

--- src/project/wwwroot/js/watchParty.js
+++ src/project/wwwroot/js/watchParty.js
@@ -1,5 +1,8 @@
 ﻿const groupId = $("#watch-group-id").val();
 const listUsers = $("#watch-group-users")[0];
+const maxUsers = $("#watch-group-total-users")[0];
+const currentUsers = $("#watch-group-current-users")[0];
+const modalBody = $(".modal-body")[0];
 
 $(".add-user").on("click", function (e) {
     e.preventDefault();
@@ -12,7 +15,14 @@ $(".add-user").on("click", function (e) {
         success: addUserToTable(e),
         error: errorOnAjax
     });
-    
+
+    if (currentUsers.value === maxUsers.value) {
+        displayNoUsers();
+    }
+});
+
+$("#test-btn").on("click", function (e) {
+    console.log(modalBody);
 });
 
 function addUserToTable(e) {
@@ -22,7 +32,24 @@ function addUserToTable(e) {
     element.innerHTML = username;
 
     listUsers.append(element);
+    updateCurrentUsers();
     removeButton(e);
+}
+
+function updateCurrentUsers() {
+    currentUsers.value = Number(currentUsers.value) + 1;
+}
+
+function displayNoUsers() {
+    const div = document.createElement("div");
+    div.setAttribute("class", "col-4");
+
+    const element = document.createElement("p");
+    element.innerHTML = "No users to add";
+
+    div.append(element);
+    modalBody.append(div);
+
 }
 
 function removeButton(e) {
