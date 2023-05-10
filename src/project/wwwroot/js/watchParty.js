cbadcb5e4f25f0c7dd176ebfb0cc9c5ec614cb38
Armando Yanez <ayanez20@wou.edu>
2023-05-10 13:05:47 -0700

    users are being successfully removed with remove users button in Watch Party Group page need to fix bug where if user is added/removed it they don't show up in the other modal to remove/add

--- src/project/wwwroot/js/watchParty.js
+++ src/project/wwwroot/js/watchParty.js
@@ -1,6 +1,7 @@
 ﻿const groupId = $("#watch-group-id").val();
 const listUsers = $("#watch-group-users")[0];
 const maxUsers = $("#watch-group-total-users")[0];
+const minUsers = 1;
 const currentUsers = $("#watch-group-current-users")[0];
 const modalBody = $(".modal-body")[0];
 
@@ -16,8 +17,25 @@ $(".add-user").on("click", function (e) {
         error: errorOnAjax
     });
 
-    if (currentUsers.value === maxUsers.value) {
-        displayNoUsers();
+    if (Number(currentUsers.value) === Number(maxUsers.value)) {
+        displayNoUsers("add");
+    }
+});
+
+$(".remove-user").on("click", function(e) {
+    e.preventDefault();
+    const userId = e.target.value;
+
+    $.ajax({
+        type: "DELETE",
+        dataType: "json",
+        url: `/partyGroup/RemoveUser/?groupId=${groupId}&userId=${userId}`,
+        success: removeUserFromTable(e),
+        error: errorOnAjax
+    });
+
+    if (Number(currentUsers.value) === minUsers) {
+        displayNoUsers("remove");
     }
 });
 
@@ -28,20 +46,46 @@ function addUserToTable(e) {
     element.innerHTML = username;
 
     listUsers.append(element);
-    updateCurrentUsers();
+    updateCurrentUsers("add");
     removeButton(e);
 }
 
-function updateCurrentUsers() {
-    currentUsers.value = Number(currentUsers.value) + 1;
+function removeUserFromTable(e) {
+    const username = e.target.innerHTML;
+    const userInTable = $(`#remove-user-${username}`)[0];
+
+    userInTable.remove();
+    updateCurrentUsers("remove");
+    removeButton(e);
 }
 
-function displayNoUsers() {
+function updateCurrentUsers(operation) {
+
+    switch (operation) {
+        case "add":
+            currentUsers.value = Number(currentUsers.value) + 1;
+            break;
+        case "remove":
+            currentUsers.value = Number(currentUsers.value) - 1;
+            break;
+    }
+
+}
+
+function displayNoUsers(operation) {
     const div = document.createElement("div");
     div.setAttribute("class", "col-4");
 
     const element = document.createElement("p");
-    element.innerHTML = "No users to add";
+
+    switch (operation) {
+        case "add":
+            element.innerHTML = "No users to add";
+            break;
+        case "remove":
+            element.innerHTML = "No users to remove";
+            break;
+    }
 
     div.append(element);
     modalBody.append(div);
