b168c4c54094f7d9dc55c8059bb72366ed329b3e
Armando Yanez <ayanez20@wou.edu>
2023-05-19 23:07:10 -0700

    beginning implementating to send sms to users started by retriving saved keys in user secrets and using them in a scoped reference later in the program

---
+++ src/project/Services/Concrete/TwilioService.cs
@@ -0,0 +1,19 @@
+﻿using WatchParty.Services.Abstract;
+
+namespace WatchParty.Services.Concrete;
+public class TwilioService: ITwilioService
+{
+    private string? _accountSid;
+    private string? _authToken;
+
+    public TwilioService(string accountSid, string authToken)
+    {
+        _accountSid = accountSid;
+        _authToken = authToken;
+    }
+
+    public void SendReminder(string recipientNumber, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
